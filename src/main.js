183c1ddb0de0bd4b6ef81d24e7d675d8468d7314
anbcodes <anbcodes@protonmail.com>
2019-08-04 16:21:46 -0400

    finished

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import vuetify from './plugins/vuetify';
+import Database from './plugins/database'
 Vue.config.productionTip = false
-
+Vue.prototype.$db = new Database()
+Vue.prototype.$bus = new Vue()
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
